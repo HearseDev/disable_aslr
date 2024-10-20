5267be0d59ca46a7df6a463f910c0a049d34f051
Hearse <28695977+HearseDev@users.noreply.github.com>
2024-10-20 12:33:28 -0700

    utilize kpf

--- Makefile
+++ Makefile
@@ -1,21 +1,20 @@
 SCRIPTS_DIR = scripts
-TOOLS_DIR = tools
 INCLUDE_DIR = include
 
-PONGOTERM = $(TOOLS_DIR)/pongoterm
+PONGOTERM = ./$(SCRIPTS_DIR)/pongoterm
 
 MODULE = disable_aslr.bin
+
+BOOT_HELPER = ./$(SCRIPTS_DIR)/boot_helper
 
 $(MODULE): main.c
 	xcrun -sdk iphoneos clang main.c -o $@ -I$(INCLUDE_DIR) -arch arm64 -mabi=aapcs -Xlinker -kext -nostdlib -Xlinker -fatal_warnings -D_SECURE__STRING_H_ -O3 -flto -ffreestanding -U__nonnull -DTARGET_OS_OSX=0 -DTARGET_OS_MACCATALYST=0 
 
 clean:
 	rm -f $(MODULE)
-
 term:
-	./$(PONGOTERM)
+	$(BOOT_HELPER) && $(PONGOTERM)
 boot:
-	CHECKRA1N_EARLY_EXIT=1 /Applications/checkra1n.app/Contents/MacOS/checkra1n -p
-
+	$(BOOT_HELPER)
 test: $(MODULE)
-	$(PONGOTERM) <cmd.txt
+	$(BOOT_HELPER) && $(PONGOTERM) <cmd.txt
--- main.c
+++ main.c
@@ -1,41 +1,111 @@
 #include "depends.h"
-#include <mach/mach.h>
-#include <pongo.h>
+#include "pongo.h"
+#include <mach-o/loader.h>
 
 extern struct mach_header_64 *xnu_header();
+static struct mach_header_64 *header = NULL;
 
-#define TARGET_OFFSET 0x00F25C28 // Offset of the target instruction
-#define TARGET_INST 0x9a9f132d   // CSEL X13, X25, XZR, NE
-#define REPLACE_INST 0xAA1F03ED  // MOV X13, XZR
+#define TARGET_INST 0x9a9f132d  // CSEL X13, X25, XZR, NE
+#define REPLACE_INST 0xAA1F03ED // MOV X13, XZR
+
+// https://github.com/checkra1n/PongoOS/blob/4c9b7541629234147fcc778f0ce4162482aaccef/checkra1n/kpf/main.c#L88C1-L100C2
+uint32_t *find_next_insn(uint32_t *from, uint32_t num, uint32_t insn,
+                         uint32_t mask) {
+  while (num) {
+    if ((*from & mask) == (insn & mask)) {
+      return from;
+    }
+    from++;
+    num--;
+  }
+  return NULL;
+}
+
+bool disable_aslr_callback_ios14(struct xnu_pf_patch *patch,
+                                 uint32_t *opcode_stream) {
+
+  uint32_t *is_patched =
+      find_next_insn(opcode_stream, 0xc, REPLACE_INST, 0xffffffff);
+  if (is_patched != NULL) {
+    print_hex64("Target instruction already patched at: ",
+                (uint64_t)is_patched - (uint64_t)header);
+    return true;
+  }
+
+  uint32_t *found = find_next_insn(opcode_stream, 0xc, TARGET_INST, 0xffffffff);
+  if (found == NULL) {
+    return false;
+  }
+
+  print_hex64("found at offset: ", (uint64_t)found - (uint64_t)header);
+  print_hex32("target: ", (uint32_t)*found);
+
+  if (*found != TARGET_INST) {
+    puts("Target instruction not found");
+    return false;
+  }
+  puts("Patching target instruction");
+  *found = REPLACE_INST;
+  puts("Patched target instruction");
+  return true;
+}
 
 void disable_aslr() {
-  struct mach_header_64 *hdr = xnu_header();
-  if (hdr == NULL) {
+  header = xnu_header();
+  if (header == NULL) {
     panic("Failed to get XNU mach header");
   }
-  print_hex64("Header: ", (uint64_t)hdr);
+  print_hex64("Header: ", (uint64_t)header);
 
-  uint64_t addr = (uint64_t)hdr + TARGET_OFFSET;
-  uint32_t target = *(uint32_t *)addr;
+  // https://github.com/checkra1n/PongoOS/blob/4c9b7541629234147fcc778f0ce4162482aaccef/checkra1n/kpf/main.c#L1628
+  xnu_pf_patchset_t *text_exec_patchset =
+      xnu_pf_patchset_create(XNU_PF_ACCESS_32BIT);
+  xnu_pf_range_t *text_exec_range =
+      xnu_pf_section(header, "__TEXT_EXEC", "__text");
+  struct mach_header_64 *first_kext = xnu_pf_get_first_kext(header);
+  if (first_kext) {
+    xnu_pf_range_t *first_kext_text_exec_range =
+        xnu_pf_section(first_kext, "__TEXT_EXEC", "__text");
 
-  print_hex32("target: ", target);
+    if (first_kext_text_exec_range) {
+      uint64_t text_exec_end_real;
+      uint64_t text_exec_end = text_exec_end_real =
+          ((uint64_t)(text_exec_range->va)) + text_exec_range->size;
+      uint64_t first_kext_p = ((uint64_t)(first_kext_text_exec_range->va));
 
-  if (target != TARGET_INST && target != REPLACE_INST) {
-    panic("Invalid target instruction");
+      if (text_exec_end > first_kext_p &&
+          first_kext_text_exec_range->va > text_exec_range->va) {
+        text_exec_end = first_kext_p;
+      }
+
+      text_exec_range->size -= text_exec_end_real - text_exec_end;
+    }
   }
 
-  if (target == REPLACE_INST) {
-    puts("already patched");
-    return;
-  }
+  uint64_t matches[] = {
+      0x121B0108, // AND W8, W8, #0x20
+      0x2A481588, // ORR W8, W12, W8,LSR#5
+      0x7100011F, // CMP W8, #0
+                  // TARGET_INST // CSEL X13, X25, XZR, NE
 
-  puts("Patching target instruction");
-  *(uint32_t *)addr = REPLACE_INST;
+  };
+  uint64_t masks[] = {
+      0xffffffff, // AND W8, W8, #0x20
+      0xffffffff, // ORR W8, W12, W8,LSR#5
+      0xffffffff, // CMP W8, #0
+                  // 0xffffffff  // CSEL X13, X25, XZR, NE
+  };
+
+  xnu_pf_maskmatch(text_exec_patchset, "disable_aslr", matches, masks,
+                   sizeof(matches) / sizeof(uint64_t), false,
+                   (void *)disable_aslr_callback_ios14);
+
+  xnu_pf_emit(text_exec_patchset);
+  xnu_pf_apply(text_exec_range, text_exec_patchset);
+  xnu_pf_patchset_destroy(text_exec_patchset);
 }
 
-void module_entry() {
-  command_register("disable_aslr", "Disable aslr", disable_aslr);
-}
+void module_entry() { disable_aslr(); }
 
 char *module_name = "disable_aslr";
 
